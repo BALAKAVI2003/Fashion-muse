c2fa9686a2a1aa844ce12145cde3f2803defd756
BALAKAVI2003 <bb4863@srmist.edu.in>
2025-03-12 21:40:56 +0530

    Initial commit

---
+++ frontend/src/pages/DirectShoppingPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ShoppingPage() {
+  return (
+    <div className="chat-container">
+      <h2>Direct Shopping Links</h2>
+      <p>Shop recommended items via integrated links to top platforms.</p>
+      {/* Display affiliate shopping links */}
+    </div>
+  );
+}
+
+export default ShoppingPage;
